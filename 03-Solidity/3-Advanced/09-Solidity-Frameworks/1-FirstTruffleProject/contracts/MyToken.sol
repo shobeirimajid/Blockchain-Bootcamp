31ffb09e01ba25e398d9a2e462e6053cd343f396
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-04-28 14:22:41 +0430

    +

---
+++ 03-Solidity/3-Advanced/09-Solidity-Frameworks/1-FirstTruffleProject/contracts/MyToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.25 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("MyToken", "MTK") {}
+
+    function safeMint(address account, uint amount) public onlyOwner {
+        _mint(account, amount);
+    }
+}
