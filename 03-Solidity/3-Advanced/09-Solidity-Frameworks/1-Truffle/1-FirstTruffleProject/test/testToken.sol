a9b7068ad2155ff12aa83d008dcc29030699455d
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-04-29 01:43:37 +0430

    +

--- 03-Solidity/3-Advanced/09-Solidity-Frameworks/1-Truffle/1-FirstTruffleProject/test/testToken.sol
+++ 03-Solidity/3-Advanced/09-Solidity-Frameworks/1-Truffle/1-FirstTruffleProject/test/testToken.sol
@@ -20,18 +20,19 @@ contract TestMyToken {
     address acc1;
     address acc2;
 
-    uint acc0_InitBalance;
+    uint acc0_InitialBalance;
 
-    constructor() {
+    constructor() {}
+
+    function beforeAll() public {
         creator = msg.sender;
-    }
 
-    function  beforeAll() public {
         acc0 = 0xa0A9F2b0b60635C75d5a935D93019e0981482B88;
         acc1 = 0xa71dcE268b90F433b019BA95c6dc67624dE917dE;
         acc2 = 0xE3a3A1396b662F7489Db4Ab07623dD9809A23cD5;
 
-        acc0_InitBalance = acc0.balance;
+        // acc0.balance - initialBalance
+        acc0_InitialBalance = acc0.balance;
 
         // owner of the MyToken is this contract ( i.e. address(this) )
         owner = address(this);
@@ -39,39 +40,42 @@ contract TestMyToken {
         mtk = new MyToken();
     }
 
-    function testIntialInfo() public {
-        console.log("TestMyToken created by:       ", creator);
-        console.log("TestMyToken contract address: ", address(this));
-        console.log("TestMyToken initialBalance:   ", address(this).balance);
 
-        Assert.equal(creator, acc0 , "incorrect creator of TestMyToken");
+    function testInitialInfo() public {
+
+        console.log("TestMyToken created by:        ", creator);
+        console.log("TestMyToken contract address:  ", address(this));
+        console.log("TestMyToken balance:           ", address(this).balance);
+
+        Assert.equal(creator, acc0, "incorrect creator of TestMyToken");
 
         // Truffle will send the TestMyToken one Ether after deploying the contract.
-        Assert.equal(address(this).balance, initialBalance , "incorrect initialBalance of TestMyToken");
+        Assert.equal(address(this).balance, initialBalance, "incorrect initialBalance of TestMyToken");  
     }
 
     // 1-Token should be deployed with correct info
     function testTokenInfo() public {
-        //MyToken mtk = MyToken(DeployedAddresses.MyToken());
+        // MyToken mtk = MyToken(DeployedAddresses.MyToken());
 
-        console.log("token name:     ", mtk.name());
-        console.log("token symbol:   ", mtk.symbol());
-        console.log("token decimals: ", mtk.decimals());
-        console.log("token owner:    ", mtk.owner());
+        console.log("token name:       ", mtk.name());
+        console.log("token symbol:     ", mtk.symbol());
+        console.log("token decimals:   ", mtk.decimals());
+        console.log("token owner:      ", mtk.owner());
 
         Assert.equal(mtk.name(), "MyToken", "Name of Token must be 'MyToken'");
         Assert.equal(mtk.symbol(), "MTK", "Symbol of Token must be 'MTK'");
         Assert.equal(mtk.decimals(), 18, "decimals of Token should be 18");
-        Assert.equal(mtk.owner(), owner , "incorrect owner of MyToken");
+        Assert.equal(mtk.owner(), owner, "incorrect owner of MyToken");
     }
 
     // 2-should mint 1000 MTK Token to the first account
     function testTokenMint() public {
-        //MyToken mtk = MyToken(DeployedAddresses.MyToken());
+        // MyToken mtk = MyToken(DeployedAddresses.MyToken());
         uint amount = 1000;
 
         // this contract (owner) is the owner of MyToken and calls safeMint to mint tokens for itself
         mtk.safeMint(owner, amount);
+
         Assert.equal(mtk.balanceOf(owner), amount, "balance of acc0 should be 1000");
         Assert.equal(mtk.totalSupply(), amount, "totalSupply of Token should be 1000");
     }
@@ -79,56 +83,61 @@ contract TestMyToken {
     // 3-should sent 200 MTK Token from the owner to the acc1
     function testTokenTransfer() public {
         // Check the initial balances of owner and acc1.
-        Assert.equal(mtk.balanceOf(owner), 1000, "");
-        Assert.equal(mtk.balanceOf(acc1), 0, "");
+        Assert.equal(mtk.balanceOf(owner), 1000, "balance of owner should be 1000");
+        Assert.equal(mtk.balanceOf(acc1), 0, "balance of acc1 should be 0");
 
         // Make transaction from address(this)/owner to acc1.
         mtk.transfer(acc1, 200);
 
-        //Check the balances of acc0 and acc1 after transaction.
-        Assert.equal(mtk.balanceOf(owner), 800, "Amount wasn't correctly taken from the acc0");
-        Assert.equal(mtk.balanceOf(acc1), 200, "Amount wasn't correctly sent to the acc1");
+        // Check the balances of owner and acc1 after transaction.
+        Assert.equal(mtk.balanceOf(owner), 800, "balance of owner should be 800");
+        Assert.equal(mtk.balanceOf(acc1), 200, "balance of acc1 should be 200");
     }
 
+
     function testFinalBalance() public {
+
         uint acc0_FinalBalance = acc0.balance;
 
-        console.log("acc[0] balance after charging contract: ", acc0_InitBalance);
-        console.log("acc[0] final ether balance:             ", acc0_FinalBalance);
-        console.log("Gas paid by accounts[0]:                ",  initialBalance - ((acc0_InitBalance + initialBalance) - acc0_FinalBalance));
-        console.log("contract final ether balance:           ", address(this).balance);
+        console.log("acc[0] balance after charging contract:    ", acc0_InitialBalance);
+        console.log("acc[0] final ether after balance:          ", acc0_FinalBalance);
+        console.log("Gas paid by acc[0]:                        ", initialBalance - ((acc0_InitialBalance + initialBalance) - acc0_FinalBalance));
+        console.log("contract final ether balance:              ", address(this).balance);
 
         // sent Ethers by Truffle must have remained in the contract after test execution.
-        Assert.equal(address(this).balance, initialBalance , "incorrect Balance of TestMyToken");
+        Assert.equal(address(this).balance, initialBalance, "incorrect Balance of TestMyToken");
     }
 
     function afterAll() public {
 
-        // Send remained tokens to acc0.
+        // check remained tokens inside the Test contract
         console.log("remained tokens inside the Test contract: ", mtk.balanceOf(owner));
 
+        // Send remained tokens to acc0.
         mtk.transfer(acc0, mtk.balanceOf(owner));
 
+        // check remained tokens inside the Test contract
         console.log("remained tokens inside the Test contract: ", mtk.balanceOf(owner));
 
-        Assert.equal(mtk.balanceOf(owner), 0, "there are tokens inside contract yet");
+        console.log("TestMyToken contract balance:  ", address(this).balance);
 
         // Send remained ethers to acc0.
         selfdestruct(payable(acc0));
     }
-
 }
 
+
 /*
-Accounts:
-(0) 0xa0a9f2b0b60635c75d5a935d93019e0981482b88
-(1) 0xa71dce268b90f433b019ba95c6dc67624de917de
-(2) 0xe3a3a1396b662f7489db4ab07623dd9809a23cd5
-(3) 0x813db98387559aba658584d11024b40fb3eb61af
-(4) 0x7a0862098799eca10daef70e94e65885831366d9
-(5) 0x9e1fd6c39661f0618ef8b081660d649408e1572a
-(6) 0xd27753a6177c4840aa78af2969a007e359f82bd7
-(7) 0xb7feaa756c82860fc7abe22fa0c463f0b1866da7
-(8) 0xefddf4007ca0ef61a2ceda7c68768495fbb9c2b1
-(9) 0xc56c8459540cce0b8056e55e53b86ad5161750db
+    Accounts:
+    (0) 0xa0a9f2b0b60635c75d5a935d93019e0981482b88
+    (1) 0xa71dce268b90f433b019ba95c6dc67624de917de
+    (2) 0xe3a3a1396b662f7489db4ab07623dd9809a23cd5
+    (3) 0x813db98387559aba658584d11024b40fb3eb61af
+    (4) 0x7a0862098799eca10daef70e94e65885831366d9
+    (5) 0x9e1fd6c39661f0618ef8b081660d649408e1572a
+    (6) 0xd27753a6177c4840aa78af2969a007e359f82bd7
+    (7) 0xb7feaa756c82860fc7abe22fa0c463f0b1866da7
+    (8) 0xefddf4007ca0ef61a2ceda7c68768495fbb9c2b1
+    (9) 0xc56c8459540cce0b8056e55e53b86ad5161750db
+
 */
